c9504232b56c634368ac633aa2dc264a017003c1
Asmita-1995 <jn.asmita@gmail.com>
2024-07-10 18:44:04 -0700

    Add source files

---
+++ src/main/java/com/gateway/MsGatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatewayServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MsGatewayServiceApplication.class, args);
+	}
+
+}
